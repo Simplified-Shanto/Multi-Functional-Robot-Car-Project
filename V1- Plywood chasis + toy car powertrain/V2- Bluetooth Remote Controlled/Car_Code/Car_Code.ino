75a3e57641b27f0260aab85bcacc56c199a43ebf
Nayeem Islam Shanto <islamnayeem386@gmail.com>
2025-06-07 21:12:53 +0600

    Stable Obstacle Avoidance Code
    Major changes:
    1. Inclusion of LCD display for changing settings of the car and visualize parameters.
    
    2. Obstacle avoidance with SG90 Servo motor and HCSR04 ultrasonic sensor

--- V1- Plywood chasis + toy car powertrain/V2- Bluetooth Remote Controlled/Car_Code/Car_Code.ino
+++ V1- Plywood chasis + toy car powertrain/V2- Bluetooth Remote Controlled/Car_Code/Car_Code.ino
@@ -1,15 +1,29 @@
+#include<Servo.h>
 //Pin declaration for left motor. 
-const int enL = 10; 
-const int in1 = 12; 
-const int in2 = 11; 
+#define enL 6 
+#define in1 12
+#define in2 11
 
 //Pin declaration for right motor. 
-const int enR = 9; 
-const int in3 = 7; 
-const int in4 = 8; 
+#define enR  5 
+#define in3  7 
+#define in4  8
+
+#define servoPin 9
+#define trigPin A0
+#define echoPin A1
+
+Servo sensorServo;
+
 
 void setup() {
   Serial.begin(9600); 
+  sensorServo.attach(servoPin); 
+  sensorServo.write(90); 
+  
+
+    pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
   
   pinMode(enL, OUTPUT); 
   pinMode(in1, OUTPUT); 
@@ -20,74 +34,188 @@ void setup() {
   pinMode(in4, OUTPUT); 
 
 }
+ 
 unsigned long preMillis = millis(); 
-int speedPWM = 150; 
-int timeThreshold = 100; 
-String command = "a"; 
-char Direction = 'a'; 
+
+int actionValue = 150; 
+int timeThreshold = 100; //After how much time we will call Stop(); 
+
+
+String command = "a";   //Command received via Serial
+char Action = 'a'; //Which action we will take. Example: Mode setup
 String speedString = "x"; 
+unsigned short carMode = 1; // 1 - Joystick driving mood
+                            // 2- Obstacle avoidance mood
+unsigned short middleDistance = 0; 
+unsigned short rightDistance = 0; 
+unsigned short leftDistance = 0; 
+unsigned short oaSpeed = 90; //PWM value while driving in obstacle avoidance (OA) mode. 
 
+/*
+ * Control Characters
+ * Forward - 'f'
+ * Backward - 'b'
+ * Left - 'l'
+ * Right - 'r'
+ * 
+ * Mode setup - 'm' 
+ * OA variables settings - '0' to last index of oaSettings array. 
+ * 
+ * Serial command print format - "Command:Value" for example: "f:120." means go forward with a pwm of 120
+ */
+ 
+ #define noSettings 5               //Number of Settings
+     //   String oaSettings[noSettings] =  {"OA BF PWM", "OA BF time", "OA LR PWM" , "OA LR time", "Safe Distance"};   //The obstacle avoidance mode settings and values. 
+ unsigned short  oaSettingsValue[noSettings] = {90,         300,           200,          150,          6 };     //Time in millisecond, Safe distance is in inch. 
+                                      /*Index:  0             1             2             3             4*/
+                                      
 void loop() {
-
+ 
    if (Serial.available()) {
     command = Serial.readStringUntil('.');  
-    Direction = command[0];
+    Action = command[0]; //The type of action the remote wants us to take. 
     speedString = command.substring(2, command.length()); 
-    speedPWM = speedString.toInt(); 
-    
-    
+    actionValue = speedString.toInt(); 
+      
     preMillis = millis(); 
 
-       
-        switch(Direction)
+        switch(Action) //Which action we are going to take. 
         {
         case 'f': 
-        {
-       goForward(speedPWM); 
+        if(carMode==1){        
+       goForward(actionValue); 
         timeThreshold = 100; 
         }
         break; 
         
         case 'b': 
-        {
-        goBackward(speedPWM); 
+        if(carMode==1){
+        goBackward(actionValue); 
         timeThreshold = 100; 
         }
         break; 
         
-        case 'l': 
-        {
-        goLeft(speedPWM);  //Turning half at a speed of going. 
+        case 'l': //Left direction
+        if(carMode==1){
+        goLeft(actionValue);  //Turning half at a speed of going. 
         timeThreshold = 20; 
         }
         break; 
         
-        case 'r': 
-        {
-        goRight(speedPWM); 
+        case 'r': //Right direction
+        if(carMode==1){
+        goRight(actionValue); 
         timeThreshold = 20;   //Turning at the half speed of going
         }
         break; 
 
-         default:
-       Stop(); 
-          
+        case 'm': //Mode setup 
+        {
+          Stop(); 
+          sensorServo.write(90); //Making the sensor front facing in the beginning of all modes
+          delay(1000); 
+          carMode = actionValue; 
         }
-       
+        break; 
+
+         default:
+      {
+        oaSettingsValue[Action - '0'] = actionValue; 
+      }     
+   } 
    }
    else 
    {
-    if(millis() - preMillis > timeThreshold)
+    if(carMode==1 && millis() - preMillis > timeThreshold) //We will stop only when in mode-1 joystick driving mode. 
     {
       Stop(); 
       preMillis = millis(); 
     }
    }
 
+    
+    if(carMode==2) //Whether car is in collission avoidance mode. 
+    {
+      middleDistance = calculateDistance(); 
+ serialPrint('M', middleDistance); 
 
+      if(middleDistance <= oaSettingsValue[4]) //oaSettingsValue[4] = safeDistance
+      {
+       Stop(); 
+       sensorServo.write(10);                //Moving US sensor to the right
+       delay(1000);                          //Keeping the sensor steady for 500 ms. 
+       rightDistance = calculateDistance();  //returns distance in inch unit
+       serialPrint('R', rightDistance);      //Sending the reading to the controller.
+       
+       
+       sensorServo.write(90);                //Moving ultrasonic sensor to the middle
+       delay(1000);                          //Keeping the sensor steady on the servo to get a stable reading. 
+       middleDistance = calculateDistance(); //Calculating front distance in inch unit
+       serialPrint('M', middleDistance);     //Sending the front distance reading to the controller. 
+       
+       
+       sensorServo.write(170); //Moving ultrasonic sensor to the left
+       delay(1000);       //Keeping the sensor steady on the servo to get a stable reading. 
+       leftDistance = calculateDistance();  //Calculating left distance. 
+       serialPrint('L', leftDistance);      //Sending the data to the controller. 
+       
  
+       sensorServo.write(90);               //Taking back the sensor on the servo to front direction 
+       delay(500);                          //Having a short delay before going to newly found direction. 
+
+
+       if(middleDistance > oaSettingsValue[4]) //oaSettingsValue[4] = safe distance . Checking whether we can proceed in the front direction 
+       {
+        goForward(oaSettingsValue[2]); 
+       // delay(50); 
+       }
+       else if(rightDistance >= oaSettingsValue[4] && rightDistance >= leftDistance)  //Whether right side has enough safe
+       {
+        goRight(oaSettingsValue[2]);  
+        delay(oaSettingsValue[3]); 
+       }
+       else if(leftDistance >= oaSettingsValue[4]) 
+       {
+        goLeft(oaSettingsValue[2]); 
+        delay(oaSettingsValue[3]); 
+       }
+      else{
+        goBackward(oaSettingsValue[0]); 
+        delay(oaSettingsValue[1]); 
+       }
+       
+      } 
+      else 
+      {
+        goForward(oaSettingsValue[0]); 
+      }
+
+    }
+    
+    
 }
 
+void serialPrint(char c, int v) //for printing message to the Seiral Monitor in the format 'action:value.'
+{
+    Serial.print(c); 
+    Serial.print(':'); 
+    Serial.print(v); 
+    Serial.print('.'); 
+}
+
+
+int calculateDistance(){ 
+  
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH); 
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+ long duration = pulseIn(echoPin, HIGH); // Reads the echoPin, returns the sound wave travel time in microseconds
+  
+  return duration*0.0068; //returns distance in inch. 
+}
 
 
 void Stop()
@@ -96,57 +224,57 @@ void Stop()
   digitalWrite(enR, LOW); 
 }
 
-void rightForward(int speedPWM) //Sets the right motor to move forward
+void rightForward(int actionValue) //Sets the right motor to move forward
 {
-  analogWrite(enR, speedPWM); 
+  analogWrite(enR, actionValue); 
    digitalWrite(in3, HIGH); 
   digitalWrite(in4, LOW); 
 }
 
-void rightBackward(int speedPWM)
+void rightBackward(int actionValue)
 {
-  analogWrite(enR, speedPWM); 
+  analogWrite(enR, actionValue); 
    digitalWrite(in3, LOW); 
   digitalWrite(in4, HIGH); 
 }
 
-void leftForward(int speedPWM)
+void leftForward(int actionValue)
 {
-analogWrite(enL, speedPWM); 
+analogWrite(enL, actionValue); 
    digitalWrite(in1, HIGH); 
   digitalWrite(in2, LOW); 
   
 }
 
-void leftBackward(int speedPWM)
+void leftBackward(int actionValue)
 {
-  analogWrite(enL, speedPWM); 
+  analogWrite(enL, actionValue); 
    digitalWrite(in1, LOW); 
   digitalWrite(in2, HIGH); 
 }
 
 
-void goForward(int speedPWM)
+void goForward(int actionValue)
 {
- rightForward(speedPWM); 
- leftForward(speedPWM); 
+ rightForward(actionValue); 
+ leftForward(actionValue); 
 }
 
-void goBackward(int speedPWM)
+void goBackward(int actionValue)
 {
-  rightBackward(speedPWM); 
-  leftBackward(speedPWM); 
+  rightBackward(actionValue); 
+  leftBackward(actionValue); 
 }
 
-void goLeft(int speedPWM)
+void goLeft(int actionValue)
 {
- rightForward(speedPWM); 
- leftBackward(speedPWM); 
+ rightForward(actionValue); 
+ leftBackward(actionValue); 
 }
 
 
-void goRight(int speedPWM)
+void goRight(int actionValue)
 {
-    leftForward(speedPWM); 
-    rightBackward(speedPWM); 
+    leftForward(actionValue); 
+    rightBackward(actionValue); 
 }
--- V1- Plywood chasis + toy car powertrain/V2- Bluetooth Remote Controlled/README.md
+++ V1- Plywood chasis + toy car powertrain/V2- Bluetooth Remote Controlled/README.md
@@ -1,8 +1,37 @@
 In this version of the car, you basically have two hardware units: the car, and the controller. 
 
-1. The car comprises of these major electrical component: Arduino UNO, L293D motor driver, HC05 bluetooth module. 
+1. The car 
+It comprises of these major electro-mechanical components: 
+a. Arduino UNO:
+Onboard microcontroller
 
-2. The controller: Arduino Nano, KY023 joystick module, HC05 bluetooth module, 16x2 Liquid crystal display. 
+b. L293D motor driver:
+Drives motors
+
+c. HC05 bluetooth module:
+Maintains wireless serial communication with controller device.
+
+d. SG90 Servo motor: 
+Ultrasonic sensor is mounted on it for changing the direction of ultrasonic beam
+
+e. HCSR04 Ultrasonic Sensor:
+Helps in measuring distance in a particular direction
+
+
+2. The controller 
+It comprises of these major electro-mechanical components:
+
+a.Arduino Nano:
+Onboard microcontroller
+
+b.KY023 joystick module:
+Used as the main controlling sensor
+
+c.HC05 bluetooth module:
+Maintains wireless communication with the car
+
+d.16x2 Liquid crystal display:
+Helps changing the settings of the car and display values of different parameters.
 
 In this version, the controller can send mode changing commands to the car, and all these processes can be monitored via the liquid crystal display. 
 
