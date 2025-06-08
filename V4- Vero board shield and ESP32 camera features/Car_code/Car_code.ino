ddce89bc7a4935a927b54b9ada3be94f54a7b1cb
Nayeem Islam Shanto <islamnayeem386@gmail.com>
2025-06-08 10:29:33 +0600

    Stable Robotic Arm Version
    1. Make the robotic arm compatible with the vero board shield

--- V4- Vero board shield and ESP32 camera features/Car_code/Car_code.ino
+++ V4- Vero board shield and ESP32 camera features/Car_code/Car_code.ino
@@ -1,8 +1,48 @@
+/*Written by: Nayeem Islam Shanto (islamshafiul283@gmail.com)
+ *Intended board: Arduino Uno.
+ * 
+ *Modification start: 15/10/2023
+ *Modification end: 
+ *
+ *Modifications done: 
+ ** Second joint from base will contain 180degree servo whose positions
+ * will be tuned by a arraylist.
+ *
+ *
+ *Modifications planned: 
+ *
+ *Give two 2 servos o the first joint actual PWM pin thus they 
+ *don't go to random position on startup. 
+ *
+ *  Updating comments according to current configurations. 
+ *Writing the code neatly in separte files. 
+ *
+ *
+ *
+ * Spare Motor channel will be assigned to the mini gear motor for the 
+ * joint before the claw. 
+ * 
+  
+ * !!!!!!Check whether the code fits to the memory size with the angles - Checked. 
+ * 
+ */
+
 #include<Servo.h>
 //Pin declaration for the 74HC595 shift register
 #define latchPin 8  //Pin 12 of 74HC595 shift register
 #define clockPin 4  //Pin 11 of the 74HC595 shift register
 #define dataPin  2 //Pin 14 of the 74HC595 shift register
+
+
+/*The two servo motors used for the first joint 
+ * doesn't align for some pair of angles. 
+ * So we have modified the angle values to 
+ * achieve the goal. 
+*/
+const byte leftAngles[177] = {2 ,    3 ,   4 ,   5 ,   6 ,   7 ,   8 ,   9 ,   10 , 11 ,   12 ,  13 ,  14 , 15 ,  16 ,  17 ,  18 ,  19 ,  20 ,  21 ,   22 , 23 ,   24 , 25 ,   26 ,  27 , 28 ,   29 ,  30 ,  31 ,  32 ,  33 ,  34 ,  35 ,  36 ,  37 , 38 ,  39 ,  40 ,  41 ,  42 ,  43 ,  44 ,  45 ,  46 ,  47 ,  48 ,   49 ,  50 ,  51 ,  52 ,  53 ,  54 , 55 ,  56 ,  57 ,  58 ,  59 ,  60 ,  61 ,  62 ,  63 ,   64 ,  65 , 66 ,  67 ,  68 ,  69 ,  70 ,  71 ,  72 ,  73 ,  74 ,  75 ,  76 ,  77 ,  78 ,  79 ,  80 ,  81 , 82 , 83 , 84 , 85 , 86 , 87 , 88 , 89 , 90 , 90 , 91 , 92 , 93 , 94 , 95 , 96 , 97 , 98 , 99 , 100 , 101 , 102 , 103 , 104 , 105 ,  106 , 107 ,  107, 108 , 109 , 110 , 111 , 112 , 113 , 114 , 115 , 116 , 117 , 118 , 119 , 120 , 121 , 122 , 123 , 124 , 125 , 125 , 126 , 127 , 128 , 129 , 130 , 131 , 132 , 133 ,133  , 134,  134 , 135 , 136 , 137 , 138 , 139 , 140 , 141 , 142 , 143 , 144 , 145 , 146 , 147 , 148 , 149 , 150 , 151 , 152 , 153 , 154 , 155 , 156 , 157 , 158 , 159 , 160 , 161 , 162 , 163 , 164 , 165 , 166 , 167, 168 , 169  , 170 , 171 , 172 , 173  };
+const byte rightAngles[177] ={178 , 177 , 176 , 175 , 174 , 173 , 172 , 171 , 170 , 169 , 168 , 167 , 166 , 165 , 164 , 163 , 162 , 161 , 160 , 159 , 158 , 157 , 156 , 155 , 154 , 153 , 152 , 151 , 150 , 149 , 148 , 147 , 146 , 145 , 144 , 143 , 142 , 141 , 140 , 139 , 138 , 137 , 136 , 135 , 134 , 133 , 132 , 131 , 130 , 129 , 128 , 127 , 126 , 125 , 124 , 123 , 122 , 121 , 120 , 119 , 118 , 117 , 116 , 115 , 114 , 113 , 112 , 111 , 110 , 109 , 108 , 107 , 106 , 105 , 104 , 103 , 102 , 101 , 100 , 99 , 98 , 97 , 96 , 95 , 94 , 93 , 92 , 91 , 90 , 89 , 88 , 87 , 86 , 85 , 84 , 83 , 82 , 81 , 80 ,  79 ,  78 ,   77 , 76 ,  75 ,  74 ,   73 ,  72 ,  71 ,  70 ,  69 ,  68 ,  67 , 66  , 65  , 64  , 63  , 62  , 61  , 60  , 59  , 58  , 57  , 56  , 55  , 54  , 53  , 52  , 51  , 50  , 49  , 48  , 47  , 46  , 45  , 44  , 43  , 42  , 41  , 40  , 39  , 38  , 37  , 36  , 35  , 34  , 33  , 32  , 31  , 30  , 29  , 28  , 27  , 26 ,  25 ,  24  , 23  , 22  , 21  , 20 ,  19 ,  18  , 17 ,  16   , 15 , 14 ,   13 , 12  , 11 ,   10 ,   9 ,  8 ,  7 ,   6  ,   5 ,   4 ,   3 ,   2  };
+byte anglePairIndex = 0; //Index of which angle is being written in the motor according 
+                         //the above two lists. 
 
 boolean extraPinState[8] = {0, 0, 0, 0, 0, 0, 0, 0}; //Initially all extra pins are turned low
 
@@ -33,13 +73,15 @@ boolean extraPinState[8] = {0, 0, 0, 0, 0, 0, 0, 0}; //Initially all extra pins 
 #define gripperIn2 18
 
 //Pin declaration for the unused 1 motor of the second l293d motor driver. 
-#define unusedMotorIn3 15
-#define unusedMotorIn4 21
+#define wristMotorIn3 15
+#define wristMotorIn4 21
 
 #define servoPin 12
 
 #define trigPin 13
 #define echoPin A0
+
+//PWM pins of arduino Uno -   3, 5, 6, 9, 10, 11
 
 Servo sensorServo;  //Servomotor carrying the sonar sensor.
 Servo armServo1; //Continuous rotation Servomotor at the base of the robotic arm
@@ -49,7 +91,7 @@ Servo armServo3; //The fourth servo motor from the base of the robotic arm
 Servo armServo4; //The fifth servo motor from the base of the robotic arm
 Servo armServo5; //The sixth servo motor from the base of the robotic arm
 
- byte servoPositions[6] = {90, 90, 90,90, 90, 90}; //Initial positions of the servo motors used in the arm
+ byte servoPositions[6] = {90, leftAngles[anglePairIndex], rightAngles[anglePairIndex], 90, 90, 90}; //Initial positions of the servo motors used in the arm
                                                  //armServo1, armServo3 and armServo4 are continuous rotation, so we set 90 degree (stop). 
 
 #define angleChangeDelay  5
@@ -71,7 +113,7 @@ void setup() {
    pinMode(commonEnablePin, OUTPUT);
    analogWrite(commonEnablePin, 0); 
 
-
+ //PWM pins in arduino Uno:  3, 5, 6, 9, 10, 11
   //Handling the servo motors
    sensorServo.attach(servoPin); 
   sensorServo.write(90); 
@@ -87,9 +129,9 @@ void setup() {
                                         //Setting the servo's to their initial positions. 
                   unsigned short moveGap = 700; //The gap between writing initial angles of each successive servos
     armServo1.write(servoPositions[0]); 
-    delay(moveGap); 
-    //armServo2Left.write(servoPositions[1]); 
-    //armServo2Right.write(servoPositions[1]); 
+    //delay(moveGap); 
+    armServo2Left.write(servoPositions[1]); 
+    armServo2Right.write(servoPositions[2]); 
     delay(moveGap); 
     armServo3.write(servoPositions[2]);
     delay(moveGap);  
@@ -222,65 +264,55 @@ void loop() {
 
         case 'v': //The left and right CSR from the base of the arm. 
         {
-          servoPositions[1]+=actionValue; //Incrementing/Decrementing. 
-          servoPositions[1] = max(servoPositions[1], 2); //Taking care of lower bound of angle
-          servoPositions[1] = min(servoPositions[1], 178); //Taking care of upper bound of angle
-          servoPositions[2]-=actionValue; //Doing the opposite for the opposite side servo motor. 
-          servoPositions[2] = max(servoPositions[2], 2); //Taking care of lower bound of angle
-          servoPositions[2] = min(servoPositions[2], 178); //Taking care of upper bound of angle
-
-          if(180 - servoPositions[1]!=servoPositions[2])
-          {
-            sensorServo.write(130); 
-            delay(500); 
-            sensorServo.write(90); 
-            delay(500); 
-          }
-          else
-          {
-          armServo2Left.write(servoPositions[1]); 
-          armServo2Right.write(servoPositions[2]); 
-          }
+            if(abs(actionValue) ==1 && anglePairIndex+actionValue>=0 && anglePairIndex+actionValue <= 176)
+            {
+              anglePairIndex+=actionValue; 
+              armServo2Left.write(leftAngles[anglePairIndex]); 
+              armServo2Right.write(rightAngles[anglePairIndex]); 
+             delay(10);  
+            }         
         }
         break; 
 
         case 'w': //Third CR servo counting from the base of the arm. 
         {
-          /*
+          
          if(actionValue==1){     armServo3.write(CRSspeed+90);       }  //moving in the forward direction. 
          else { armServo3.write(90-CRSspeed); }  //moving in the backward direction
          delay(CRSruntime);   //for a short time. 
-         armServo3.write(90); //then stopping the motor from rotating. 
-          */
-          servoPositions[3]+=actionValue; 
-         if(servoPositions[3]>=2 && servoPositions[3]<=178)
-         {
-          armServo3.write(servoPositions[3]); 
-          delay(60); 
-         }
-         else
-         {
-          servoPositions[3]-=actionValue; 
-         }
+         armServo3.write(90); //then stopping the motor from rotating.
+
         }
         break; 
 
         case 'x': //Fourth servo counting from the base of the arm. 
         {
+          /*  //Currently there's no motor as the fourth motor. 
            if(actionValue==1){     armServo4.write(CRSspeed+90);       }  //moving in the forward direction. 
          else { armServo4.write(90-CRSspeed); }  //moving in the backward direction
          delay(CRSruntime);   //for a short time. 
          armServo4.write(90); //then stopping the motor from rotating. 
+         */
         }
         break; 
 
         case 'y': //Fifth servo motor. 
         {
       
-           servoPositions[5]+=2*actionValue; //Incrementing/Decrementing. 
-          servoPositions[5] = max(servoPositions[5], 2); //Taking care of lower bound of angle
-          servoPositions[5] = min(servoPositions[5], 178); //Taking care of upper bound of angle
-          armServo5.write(servoPositions[5]);
+           if(actionValue == 1)
+          {
+            DigitalWrite(wristMotorIn3, HIGH); 
+            DigitalWrite(wristMotorIn4, LOW); 
+            delay(10); 
+            DigitalWrite(wristMotorIn3, LOW); 
+          }
+          else if(actionValue == -1)
+          {
+            DigitalWrite(wristMotorIn3, LOW); 
+            DigitalWrite(wristMotorIn4, HIGH); 
+            delay(10); 
+            DigitalWrite(wristMotorIn4, LOW); 
+          }
         
         }
         break;
